211f5dbcae7a54f36ae3f2794f9ade70e07af2ec
nickholmstyle <nick416holm@protonmail.com>
2022-06-28 23:59:56 +0000

    adding practice files

---
+++ whichSchool.js
@@ -0,0 +1,14 @@
+const age = 24;
+
+const whichSchool = function (age) {
+  if (age < 13); {
+    console.log("Elementary School");
+  }
+  else if (age >= 13 && age <=18); {
+    console.log("Secondary School");
+  } else {
+    console.log("Lighthouse Labs");
+  }
+}
+
+console.log(whichSchool);
